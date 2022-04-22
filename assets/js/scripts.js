80c2f69028e177ca06e8054d103aac7b5424d7c5
manuelbieri <manuel.bieri@outlook.com>
2022-04-22 10:05:19 +0200

    Update content and links in legal and index.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,9 @@
 function getDOILink(doiNr) {
-    document.write(', DOI: <a class="link" target="_blank" href="https://doi.org/' + doiNr + '">' + doiNr + '<i class="bi bi-box-arrow-up-right icon"></i></a>' )
+    document.write(', DOI: <a class="link" target="_blank" href="https://doi.org/' + doiNr + '">' + doiNr + '<i class="bi bi-box-arrow-up-right icon-link"></i></a>' )
 }
 function getExternalLink(name, url) {
-    document.write('<a class="link" target=_blank href="' + url + '">' + name + '<i class="bi bi-box-arrow-up-right icon"></i></a>')
+    document.write('<a class="link" target=_blank href="' + url + '">' + name + '<i class="bi bi-box-arrow-up-right icon-link"></i></a>')
+}
+function getInternalLink(name, url) {
+    document.write('<a class="link" href="' + url + '">' + name + '</a>')
 }
