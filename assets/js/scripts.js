854d42b0628969075c5bc636500254e566a36fc6
manuelbieri <manuel.bieri@outlook.com>
2022-04-22 15:22:24 +0200

    Update list with publications.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,7 @@
-function getDOILink(doiNr) {
+function getDOILink(doiNr, style_class = 'publication-item') {
     let doiLink = doiNr.replace('&shy;', '')
-    document.write(', DOI: <a class="link" target="_blank" href="https://doi.org/' + doiLink + '">' + doiNr + '<i class="bi bi-box-arrow-up-right icon-link"></i></a>' )
+    document.write(', DOI: <a class="' + style_class +'" target="_blank" href="https://doi.org/' + doiLink + '">' + doiNr +
+        '<i class="bi bi-box-arrow-up-right icon-link"></i></a>' )
 }
 function getExternalLink(name, url) {
     document.write('<a class="link" target=_blank href="' + url + '">' + name + '<i class="bi bi-box-arrow-up-right icon-link"></i></a>')
