54c1dc1aade38f5a2825be26f2b7fd69271ac469
Manuel Bieri <mail@manuelbieri.ch>
2023-07-27 11:08:51 +0200

    Update bootstrap, popper.js and jquery.
    Version updates require adjustments to navbar.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,24 @@
+function resetNavbar(){
+    const element = document.getElementById("navbarSupportedContent");
+    element.classList.remove("show")
+    element.classList.add("collapse");
+}
+
+function resetToggler() {
+    const button = document.getElementById("navbar-button");
+    button.blur();
+}
+
+$(window).scroll(function() {
+    resetNavbar();
+    resetToggler();
+});
+
+$(window).click(function() {
+    resetNavbar();
+});
+
+function setYear() {
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+}
 
